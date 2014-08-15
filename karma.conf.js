f2de07cd24c3563bad1c288e675b5c81441c2d7e
Justin Reidy <jmreidy@rzrsharp.net>
2014-08-15 10:25:22 -0700

    updating ci to use sauce

--- karma.conf.js
+++ karma.conf.js
@@ -1,74 +1,90 @@
 var proxyquire = require('proxyquireify');
+var _ = require('lodash');
+
+var baseConfig = {
+  basePath: '',
+
+  frameworks: [
+    'mocha',
+    'chai',
+    'sinon',
+    'browserify'
+  ],
+
+  files: [
+  ],
+
+  exclude: [],
+
+  browserify: {
+    files: [
+      'test/*.test.js'
+    ],
+    transform: ['reactify'],
+    plugin: [proxyquire.plugin]
+  },
+
+  preprocessors: {
+    '/**/*.browserify': 'browserify'
+  },
+
+
+  reporters: ['spec'],
+
+  colors: true,
+
+  autoWatch: true,
+
+  browsers: ['Chrome'],
+
+  singleRun: false,
+
+  //npm auto-loads plugins
+};
+
+var customLaunchers = {
+  sl_chrome: {
+    base: 'SauceLabs',
+    browserName: 'chrome',
+    platform: 'Windows 7',
+    version: '35'
+  },
+  sl_firefox: {
+    base: 'SauceLabs',
+    browserName: 'firefox',
+    version: '30'
+  },
+  sl_ios_safari: {
+    base: 'SauceLabs',
+    browserName: 'iphone',
+    platform: 'OS X 10.9',
+    version: '7.1'
+  },
+  sl_ie_11: {
+    base: 'SauceLabs',
+    browserName: 'internet explorer',
+    platform: 'Windows 8.1',
+    version: '11'
+  }
+};
 
 module.exports = function (config) {
-  var customLaunchers = {
-    sl_chrome: {
-      base: 'SauceLabs',
-      browserName: 'chrome',
-      platform: 'Windows 7',
-      version: '35'
-    },
-    sl_firefox: {
-      base: 'SauceLabs',
-      browserName: 'firefox',
-      version: '30'
-    },
-    sl_ios_safari: {
-      base: 'SauceLabs',
-      browserName: 'iphone',
-      platform: 'OS X 10.9',
-      version: '7.1'
-    },
-    sl_ie_11: {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows 8.1',
-      version: '11'
-    }
-  };
+  baseConfig.logLevel = config.LOG_INFO;
 
-  var browsers = (process.env.NODE_ENV === 'ci')? ['Firefox'] : ['Chrome'];
+  if (process.env.NODE_ENV === 'ci') {
+    config.set(_.extend(baseConfig, {
+      autoWatch: false,
+      browsers: Object.keys(customLaunchers),
+      reports: ['spec', 'saucelabs'],
+      singleRun: true,
+      customLaunchers: customLaunchers,
+      sauceLabs: {
+        testName: 'React-Combobox Unit Tests'
+      }
+    }));
+  }
+  else {
+    config.set(baseConfig);
+  }
 
-  config.set({
-    basePath: '',
-
-    frameworks: [
-      'mocha',
-      'chai',
-      'sinon',
-      'browserify'
-    ],
-
-    files: [
-    ],
-
-    exclude: [],
-
-    browserify: {
-      files: [
-        'test/*.test.js'
-      ],
-      transform: ['reactify'],
-      plugin: [proxyquire.plugin]
-    },
-
-    preprocessors: {
-      '/**/*.browserify': 'browserify'
-    },
-
-
-    reporters: ['spec'],
-
-    logLevel: config.LOG_DEBUG,
-
-    colors: true,
-
-    autoWatch: true,
-
-    browsers: browsers,
-
-    singleRun: (process.env.NODE_ENV === 'ci'),
-
-    //npm auto-loads plugins
-  });
 };
